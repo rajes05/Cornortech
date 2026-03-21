0b2354e21e7fc5e459929dbc3e3aa70481b16737
rajesh-rana01 <rajeshra5naa@gmail.com>
2026-03-21 09:14:48 +0545

    add seo metadata and opengraph tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,15 +10,63 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title:{
-    default: "Cornor Tech Pvt. Ltd. | Where Tech Meets Solution | Tilottama, Butwal< Nepal",
+    default: "Cornor Tech Pvt. Ltd. | Where Tech Meets Solution | Tilottama, Butwal, Nepal",
     template: '%s | Cornor Tech',
   }, 
   description: "CornorTech turns ideas into powerful digital solutions. We deliver creative, reliable, and scalable tech services.",
-  icons:{
+  keywords: [
+    'web development Nepal',
+    'mobile app development',
+    'UI UX design',
+    "digital merketing agency",
+    'cloud solutions',
+    'e-commerce development',
+    'Cornor Tech',
+    'software company Nepal',
+  ],
+  authors: [{name: 'Cornor Tech printTreeView. Ltd'}],
+  creator: 'Cornor Tech Pvt. Ltd',
+  metadataBase: new URL ('https://cornortech.com'), // real domain
+  alternates: {
+    canonical: '/',
+  },
+   icons:{
     icon:"/logo.png",
     shortcut:"/logo.png",
     apple:"/logo.png",
-  }
+  },
+  // when it shared on social media
+  openGraph: {
+    title: 'Cornor Tech Pvt. Ltd | Web Development $ Digital Solutions',
+    description:'PM-led tech teams for web, mobile, design, and digital marketing.',
+    url:'https://cornortech.com',
+    siteName: 'Cornor Tech',
+    images: [
+      {
+        url: '/logo.png', // For search engine
+        width: 1200,
+        height: 630,
+        alt: 'Cornor Tech Pvt. Ltd',
+      },
+    ],
+    locale: 'en_NP',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Cornor Tech Pvt. Ltd',
+    description: 'PM-led tech teams for web, mobile, design, and digital marketing.',
+    images: ['/logo.png'],
+  },
+  robots:{
+    index: true,
+    follow:true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-image-preview': 'large',
+    },
+  },
 };
 
 export default function RootLayout({
